45e3e169bc08df87a08b6ff481c55583a587c314
PriliviaRF <prilivia98@gmail.com>
2017-04-08 20:11:55 +0700

    Refine main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,21 +13,13 @@
 
 
 		// to ease when click '#' url
-		$(function() {
-            $('a[href*=#]:not([href=#])').click(function() {
-                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		$(document).on('click', 'a', function(event){
+		    event.preventDefault();
 
-                    var target = $(this.hash);
-                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                    if (target.length) {
-                        $('html,body').animate({
-                            scrollTop: target.offset().top
-                        }, 1000);
-                        return false;
-                    }
-                }
-            });
-        });
+		    $('html, body').animate({
+		        scrollTop: $( $.attr(this, 'href') ).offset().top
+		    }, 1000);
+		});
 
 		//owl carousel
 		var owl = $('.carousel-1');
